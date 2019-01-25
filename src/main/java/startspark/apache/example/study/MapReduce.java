11cf95ef1cfe0699fa4d4e334a5fe24ed5d6655b
Blake Rogers <saltyfog@gmail.com>
2019-01-24 16:04:23 -0800

    example of a simple map-reduce concept algorithm in which a list of 6 entries is reduced to a map of 3 entries.

---
+++ src/main/java/startspark/apache/example/study/MapReduce.java
@@ -0,0 +1,38 @@
+package startspark.apache.example.study;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapReduce {
+
+    public static void main(String args[]){
+
+        //list of 6 entries
+        List<String> items = new ArrayList();
+        items.add("bob");
+        items.add("susan");
+        items.add("julie");
+        items.add("bob");
+        items.add("bob");
+        items.add("julie");
+
+        //map reduces list of six to 3 entries
+        Map<String, Integer> map = new HashMap();
+        for(String s: items){
+            if(map.containsKey(s)){
+                Integer i = map.get(s);
+                map.put(s, i+1);
+            }else{
+                map.put(s, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+
+    }
+}
