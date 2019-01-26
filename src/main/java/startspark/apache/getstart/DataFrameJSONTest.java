2e6ddcc11ff6b43291f5ef89ea3edac97c00600d
Blake Rogers <saltyfog@gmail.com>
2019-01-25 17:42:55 -0800

    clean up code and add dataframejsontester

--- src/main/java/startspark/apache/getstart/DataFrameJSONTest.java
+++ src/main/java/startspark/apache/getstart/DataFrameJSONTest.java
@@ -1,12 +1,12 @@
 package startspark.apache.getstart;
 // http://spark.apache.org/docs/2.4.0/sql-getting-started.html#creating-dataframes
-//Untyped Dataset Operations
+
 import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-public class DataFrameOperations {
+public class DataFrameJSONTest {
 
     private static String dir = "C:\\Users\\wolf\\IdeaProjects\\startspark\\";
     private static String inputFile = "SOURCE3.json";
@@ -24,16 +24,6 @@ public class DataFrameOperations {
         Dataset<Row> df = dfr.json(inputURL);
         System.out.println("df.show()");
         df.show();
-        // Print the schema in a tree format
-        System.out.println("df.printSchema()");
-        df.printSchema();
-        //other dataframe operations
-        System.out.println("df.select(\"base\").show()");
-        df.select("date").show();
-        System.out.println("df.groupBy(\"date\").count().show()");
-        df.groupBy("date").count().show();
-
         spark.close();
     }
-
 }
