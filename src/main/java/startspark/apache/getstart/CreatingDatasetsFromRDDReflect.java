9d07da5c933f632b41211640e6586f9eda7bacaa
Blake Rogers <saltyfog@gmail.com>
2019-01-28 11:09:58 -0800

    creating datasets from a datasource using sql and a class with mapping building RDD

---
+++ src/main/java/startspark/apache/getstart/CreatingDatasetsFromRDDReflect.java
@@ -0,0 +1,96 @@
+package startspark.apache.getstart;
+// http://spark.apache.org/docs/2.4.0/sql-getting-started.html
+
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoder;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.apache.spark.sql.Row;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+
+public class CreatingDatasetsFromRDDReflect {
+
+    private static String dir = "C:\\Users\\wolf\\IdeaProjects\\startspark\\";
+    private static String inputFile = "people.txt";
+    private static String inputURL = dir+inputFile;
+
+    public static void main (String []args ){
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Create Data Frame Dataset from RDD")
+                .config("spark.master", "local")
+                .getOrCreate();
+
+
+        // Create an RDD of Person objects from a text file
+        JavaRDD<Person> peopleRDD = spark.read()
+                .textFile(inputURL)
+                .javaRDD()
+                .map(line -> {
+                    String[] parts = line.split(",");
+                    Person person = new Person();
+                    person.setName(parts[0]);
+                    person.setAge(Integer.parseInt(parts[1].trim()));
+                    return person;
+                });
+
+
+        // Apply a schema to an RDD of JavaBeans to get a DataFrame
+        Dataset<Row> peopleDF = spark.createDataFrame(peopleRDD, Person.class);
+        peopleDF.show();
+// Register the DataFrame as a temporary view
+        peopleDF.createOrReplaceTempView("people");
+// SQL statements can be run by using the sql methods provided by spark
+        Dataset<Row> teenagersDF = spark.sql("SELECT name FROM people WHERE age BETWEEN 13 AND 19");
+        teenagersDF.show();
+
+// The columns of a row in the result can be accessed by field index
+        Encoder<String> stringEncoder = Encoders.STRING();
+        Dataset<String> teenagerNamesByIndexDF = teenagersDF.map(
+                (MapFunction<Row, String>) row -> "Name: " + row.getString(0),
+                stringEncoder);
+        teenagerNamesByIndexDF.show();
+
+
+
+// or by field name
+        Dataset<String> teenagerNamesByFieldDF = teenagersDF.map(
+                (MapFunction<Row, String>) row -> "Name: " + row.<String>getAs("name"),
+                stringEncoder);
+        teenagerNamesByFieldDF.show();
+
+
+        spark.close();
+    }
+
+    public static class Person implements Serializable {
+
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+    }
+}
